297c871cd85a82716a9a2959fb4197ebe707adae
sabriskay <sabrinakay99@hotmail.com>
2015-08-08 19:06:40 -0300

    Added android studio project

---
+++ android-final-project/app/src/main/java/com/example/mybaby/android_final_project/Activities/SplashActivity.java
@@ -0,0 +1,9 @@
+package com.example.mybaby.android_final_project.Activities;
+
+import android.app.Activity;
+
+/**
+ * Created by SabrinaKay on 08/08/2015.
+ */
+public class SplashActivity extends Activity {
+}
